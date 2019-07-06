f4274257a5ee5180d7ce2630573a29af25d992ec
kikesan <kikin1601@hotmail.com>
2019-07-05 23:13:17 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-let pintar = (json)=>{
-  document.getElementById('root').innerHTML = `<ul id="ulItems"></ul>`
-  for(let i=0; i<json.length;i++){
-    console.log(json[i]);
-    
-    document.getElementById('ulItems').innerHTML += `<li><a>
-    <h2>${json[i].name}</h2>
-    <p>${json[i].email}</p>
-    <p>${json[i].phone}</p>
-    </a></li>`
+let pintar = num => {
+  if(num){
+    $('#root').html(`
+    <div class="container wrapper-header">
+      <div class="wrapper-headerUser"><span>Agregar usuario (1-12):</span><span class="descUser">Ingresar el número de id del usuario</span> </div><input class="input" type="text" id="numUser"> <a class="button is-link" id="refresh">Mostrar Usuario</a>
+    </div>
+    <div class="container wrapper-body">
+    </div>
+    `)
+    $('#refresh').click(()=>{
+      build($('#numUser').val())
+    })
   }
+}
+
+let build = (numuser)=>{
+  console.log('pintar');
+
+  $.ajax({
+    url: `https://reqres.in/api/users/${numuser}`,
+    type: "GET",
+    success: function(response){
+        console.log(response.data);
+        let d=response.data
+        $('.wrapper-body').append(`
+        <div class="item-user">
+        <img src="${d.avatar}"/>
+        <h2>${d.first_name} ${d.last_name}</h2>
+        <p>${d.email}</p>
+        </div>
+        `)
+    }
+  });
+
+  /* avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
+  email: "george.bluth@reqres.in"
+  first_name: "George"
+  id: 1
+  last_name: "Bluth" */
+  
 }
 export default pintar;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import pintar from './App'
 import "./styles/index.scss";
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response => response.json())
-.then(json =>pintar(json))
+
+$('.copyuser').click(function(){
+  //copyToClipboard($(this).text())
+  $('.username').val($(this).text())
+})
+$('.copypass').click(function(){
+  $('.password').val($(this).text())
+})
+
+
+$('#on-app').click(function(){
+
+  /* fetch('http://wsruc.com/Ruc2WS_JSON.php?tipo=2&ruc=20430635612&token=cXdlcnR5bGFtYXJja19zYUBob3RtYWlsLmNvbXF3ZXJ0eQ==')
+  .then(response => response.json())
+  .then(json => console.log(response)) */
+
+  $.ajax({
+    url: "https://reqres.in/api/login",
+    type: "POST",
+    data: {
+      "email": $('.username').val(),
+      "password": $('.password').val()
+    },
+    success: function(response){
+        console.log(response);
+        pintar(response)
+    }
+  });
+
+})
+
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
   
